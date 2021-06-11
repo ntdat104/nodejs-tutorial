a9c9bbab34e079e899eeb8d3f5f97e30a3f55d90
ntdat104 <ntd.k108cva@gmail.com>
2021-06-11 13:27:28 +0700

    update

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,69 +1,69 @@
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 function readDB() {
-    return JSON.parse(fs.readFileSync('src/db.json', { encoding: 'utf8' }));
+  return JSON.parse(fs.readFileSync("src/db.json", { encoding: "utf8" }));
 }
 
 function writeDB(value) {
-    const valueString = JSON.stringify(value);
-    fs.writeFileSync('src/db.json', valueString, {
-        encoding: 'utf8',
-    });
+  const valueString = JSON.stringify(value);
+  fs.writeFileSync("src/db.json", valueString, {
+    encoding: "utf8",
+  });
 }
 
 const POSTS = readDB();
 
 module.exports.getAllPosts = (req, res) => {
-    res.status(200).json(POSTS);
+  res.status(200).json(POSTS);
 };
 
 module.exports.getPost = (req, res) => {
-    const { id } = req.params;
-    const postSearch = POSTS.find((post) => post.id == id);
-    res.status(200).json(postSearch);
+  const { id } = req.params;
+  const postSearch = POSTS.find((post) => post.id == id);
+  res.status(200).json(postSearch);
 };
 
 module.exports.createPost = (req, res) => {
-    const { title, content } = req.body;
-    const date = new Date();
-    const post = {
-        id: uuidv4(),
-        title: title || 'title',
-        content: content || 'content',
-        createAt: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-    };
-    const newPOSTS = [...POSTS, post];
-    writeDB(newPOSTS);
-    res.status(201).json(post);
+  const { title, content } = req.body;
+  const date = new Date();
+  const post = {
+    id: uuidv4(),
+    title: title || "title",
+    content: content || "content",
+    createAt: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+  };
+  const newPOSTS = [...POSTS, post];
+  writeDB(newPOSTS);
+  res.status(201).json(post);
 };
 
 module.exports.deletePost = (req, res) => {
-    const { id } = req.params;
-    let postDelete = {};
-    const newPOSTS = POSTS.filter((post) => {
-        if (post.id === id) {
-            postDelete = post;
-        }
-        return post.id !== id;
-    });
-    writeDB(newPOSTS);
-    res.json(postDelete);
+  const { id } = req.params;
+  let postDelete = {};
+  const newPOSTS = POSTS.filter((post) => {
+    if (post.id === id) {
+      postDelete = post;
+    }
+    return post.id !== id;
+  });
+  writeDB(newPOSTS);
+  res.json(postDelete);
 };
 
 module.exports.updatePost = (req, res) => {
-    const { id } = req.params;
-    const { title, content } = req.body;
-    const newPOSTS = [];
-    let updatePost = {};
-    POSTS.forEach((post) => {
-        if (post.id === id) {
-            post.title = title || 'title';
-            post.content = content || 'content';
-            updatePost = post;
-        }
-        newPOSTS.push(post);
-    });
-    writeDB(newPOSTS);
-    res.json(updatePost);
+  const { id } = req.params;
+  const { title, content } = req.body;
+  const newPOSTS = [];
+  let updatePost = {};
+  POSTS.forEach((post) => {
+    if (post.id === id) {
+      post.title = title || "title";
+      post.content = content || "content";
+      updatePost = post;
+    }
+    newPOSTS.push(post);
+  });
+  writeDB(newPOSTS);
+  res.json(updatePost);
 };
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,27 +1,21 @@
-const express = require('express');
-const {
-    getAllPosts,
-    getPost,
-    createPost,
-    deletePost,
-    updatePost,
-} = require('../controllers/post');
+const express = require("express");
+const { getAllPosts, getPost, createPost, deletePost, updatePost } = require("../controllers/post");
 
 const router = express.Router();
 
 //TODO get all POSTS
-router.get('/', getAllPosts);
+router.get("/", getAllPosts);
 
 //TODO get 1 post
-router.get('/:id', getPost);
+router.get("/:id", getPost);
 
 //TODO createPost
-router.post('/', createPost);
+router.post("/", createPost);
 
 //TODO deletePost
-router.delete('/:id', deletePost);
+router.delete("/:id", deletePost);
 
 //TODO updatePost
-router.patch('/:id', updatePost);
+router.patch("/:id", updatePost);
 
 module.exports = router;
